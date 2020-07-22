152fd12e8602c7b46dc197e161b935c0b53c9ee6
zhangdaquan <zhangdaquan@xiaomi.com>
2020-07-22 17:44:15 +0800

    2020年07月22日 新增函数式编程-深入了解@FunctionalInterface

--- src/main/java/com/daquan/_202007/_01/function/MyFunInterface.java
+++ src/main/java/com/daquan/_202007/_01/function/MyFunInterface.java
@@ -1,6 +1,22 @@
 package com.daquan._202007._01.function;
 
-@FunctionalInterface
+@FunctionalInterface//做编译检查用的，判断当前接口是否符合函数式编程-lambda表达式
 interface MyFunInterface<K,V> {
+    int i = 1;
     V doubleNum(K i);
+    String toString(); //same to Object.toString
+    int hashCode(); //same to Object.hashCode
+    boolean equals(Object obj); //same to Object.equals
+
+    default void doSomeMoreWork1()
+    {
+        // Method body
+        System.out.println("doSomeMoreWork1");
+    }
+
+    default int doSomeMoreWork2()
+    {
+        // Method body
+        return 1;
+    }
 }
--- src/main/java/com/daquan/_202007/_01/function/TestMyFunInterface.java
+++ src/main/java/com/daquan/_202007/_01/function/TestMyFunInterface.java
@@ -5,7 +5,11 @@ import org.junit.Test;
 public class TestMyFunInterface {
     @Test
     public void test(){
-        MyFunInterface<Integer,Double> d = (i)->{return i.doubleValue();};
+        MyFunInterface<Integer,Double> d = (i)->{return Double.valueOf(i.toString());};
         System.out.println(d.doubleNum(10));
+        System.out.println(MyFunInterface.i);
+        int i = d.doSomeMoreWork2();
+        System.out.println(i);
+        d.doSomeMoreWork1();
     }
 }
